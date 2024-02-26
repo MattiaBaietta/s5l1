87198b53172c7b9d5e9f95ba38b6ef68c703bf06
MattiaBaietta <mattia.baietta89@gmail.com>
2024-02-26 16:27:54 +0100

    Aggiungere i file di progetto.

---
+++ Controllers/AddEmployeeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace s5l1.Controllers
+{
+    public class AddEmployeeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/AddPayementsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace s5l1.Controllers
+{
+    public class AddPayementsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
